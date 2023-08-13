fc2a81cf685e0c7baccfa3b43beb9023c6ffbaaa
JosephLee0 <104315224+JosephLee0@users.noreply.github.com>
2023-08-13 16:54:00 -0400

    Add files via upload
    Final Commit
    All solutions is inside module-9-challenge

---
+++ Module-9-challenge/queries.sql
@@ -0,0 +1,68 @@
+--Question 1
+select e.emp_no as "employee number", e.last_name as "last name",e.first_name as "first name",e.sex,s.salary
+from employees as e
+join salaries as s
+on e.emp_no=s.emp_no;
+
+--Question 2
+select first_name as "first name",last_name as "last name",hire_date as "hire date"
+from employees
+where extract(year from employees.hire_date ) ='1986';
+
+--Question 3
+select d.dept_no, de.dept_name, e.emp_no,e.last_name,e.first_name
+from employees as e
+join dept_manager as d
+on e.emp_no=d.emp_no
+join department as de
+on d.dept_no=de.dept_no;
+
+--Question 4
+select d.dept_no, e.emp_no,e.last_name,e.first_name, de.dept_name
+from employees as e
+join dept_emp as d
+on e.emp_no=d.emp_no
+join department as de
+on d.dept_no=de.dept_no;
+
+--Question 5
+select first_name,last_name,sex
+from employees
+where first_name='Hercules' and last_name like 'B%';
+
+--Question 6
+select emp_no, last_name,first_name 
+from employees
+where emp_no in (
+	select emp_no
+	from dept_emp
+	where dept_no in (
+		select dept_no
+		from department
+		where dept_name='Sales'
+	)
+);
+
+
+--Question 7
+select e.emp_no, e.last_name, e.first_name,de.dept_name
+from employees as e
+join dept_emp as d
+on e.emp_no=d.emp_no
+join department as de
+on d.dept_no=de.dept_no
+where dept_name in ('Sales','Development')
+
+--Question 8
+select last_name, count(*) as "frequency count"
+from employees
+group by last_name
+order by "frequency count" desc;
+
+
+
+
+
+
+
+
---
+++ Module-9-challenge/table schemata.sql
@@ -0,0 +1,76 @@
+-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "department" (
+    "dept_no" VARCHAR(10)   NOT NULL,
+    "dept_name" VARCHAR(30)   NOT NULL,
+    CONSTRAINT "pk_department" PRIMARY KEY (
+        "dept_no"
+     )
+);
+
+CREATE TABLE "dept_emp" (
+    "emp_no" INTEGER   NOT NULL,
+    "dept_no" VARCHAR(10)   NOT NULL
+);
+
+CREATE TABLE "dept_manager" (
+    "dept_no" VARCHAR(10)   NOT NULL,
+    "emp_no" INTEGER   NOT NULL
+);
+
+CREATE TABLE "employees" (
+    "emp_no" INTEGER   NOT NULL,
+    "emp_title" VARCHAR(30)   NOT NULL,
+    "birth_date" VARCHAR(10)   NULL,
+    "first_name" VARCHAR(30)   NOT NULL,
+    "last_name" VARCHAR(30)   NOT NULL,
+    "sex" VARCHAR(1)   NOT NULL,
+    "hire_date" VARCHAR(10)   NULL,
+    CONSTRAINT "pk_employees" PRIMARY KEY (
+        "emp_no"
+     )
+);
+
+CREATE TABLE "salaries" (
+    "emp_no" INTEGER   NOT NULL,
+    "salary" INTEGER   NOT NULL
+);
+
+CREATE TABLE "title" (
+    "title_id" VARCHAR(10)   NOT NULL,
+    "title" VARCHAR(30)   NOT NULL,
+    CONSTRAINT "pk_title" PRIMARY KEY (
+        "title_id"
+     )
+);
+
+ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "department" ("dept_no");
+
+ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "department" ("dept_no");
+
+ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "employees" ADD CONSTRAINT "fk_employees_emp_title" FOREIGN KEY("emp_title")
+REFERENCES "title" ("title_id");
+
+ALTER TABLE "salaries" ADD CONSTRAINT "fk_salaries_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+
+--Modifying the birth_date and the hire_date column to date data type
+UPDATE employees SET "birth_date" = TO_DATE("birth_date", 'MM/DD/YYYY'), "hire_date" = TO_DATE("hire_date", 'MM/DD/YYYY');
+ALTER TABLE employees ADD newdate1 date NULL;
+ALTER TABLE employees ADD newdate2 date NULL;
+UPDATE employees SET newdate1 = cast (birth_date as date),newdate2 = cast (hire_date as date);
+ALTER TABLE employees DROP COLUMN birth_date;
+ALTER TABLE employees DROP COLUMN hire_date;
+ALTER TABLE employees RENAME COLUMN newdate1 to birth_date;
+ALTER TABLE employees RENAME COLUMN newdate2 to hire_date;
